4c504e987118d61ee75930ce85f71e574ec9c5b8
Marcmoore <mahaoxun@gmail.com>
2015-11-12 00:53:08 +0800

    share to wechat

--- university/assets/js/all.js
+++ university/assets/js/all.js
@@ -344,4 +344,44 @@ $(document).ready(function(){
 			$(this).addClass("musicEnd")
 		}
 	});
+	
+	var shareTitle = "下一站，top10!",
+		descContent = "下一站，top10!",
+		lineLink = window.location.href,
+		imgUrl = "http://" + window.location.host + "/assets/images/logo-white.png";
+	
+	wx.onMenuShareAppMessage({
+		title: shareTitle, // 分享标题
+		desc: descContent, // 分享描述
+		link: lineLink, // 分享链接
+		imgUrl: imgUrl, // 分享图标
+		type:'',
+		dataUrl:''
+	});
+
+	wx.onMenuShareQZone({
+		title: shareTitle, // 分享标题
+		desc: descContent, // 分享描述
+		link: lineLink, // 分享链接
+		imgUrl: imgUrl, // 分享图标
+		success: function () {
+			// 用户确认分享后执行的回调函数
+		},
+		cancel: function () {
+			// 用户取消分享后执行的回调函数
+		}
+	});
+
+	wx.onMenuShareQQ({
+		title: shareTitle, // 分享标题
+		desc: descContent, // 分享描述
+		link: lineLink, // 分享链接
+		imgUrl: imgUrl, // 分享图标
+		success: function () {
+			// 用户确认分享后执行的回调函数
+		},
+		cancel: function () {
+			// 用户取消分享后执行的回调函数
+		}
+	});
 });
