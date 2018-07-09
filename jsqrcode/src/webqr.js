84b94e0c397b78873b4e5ca74c82c4f03b21a2eb
Mark <mahaoxun@gmail.com>
2018-07-09 20:51:44 +0800

    fix bug

--- jsqrcode/src/webqr.js
+++ jsqrcode/src/webqr.js
@@ -197,7 +197,7 @@ function setwebcam2(options)
 
     if(n.mediaDevices.getUserMedia)
     {
-        options = { facingMode: "environment"};
+        options = { facingMode: { exact: 'environment' } };
         alert(1);
         n.mediaDevices.getUserMedia({video: options, audio: false}).
             then(function(stream){
